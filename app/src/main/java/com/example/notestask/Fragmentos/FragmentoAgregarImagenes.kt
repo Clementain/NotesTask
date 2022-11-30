c57d19b4f11af7ae1f6ec677be901b2993fab842
Clementain <clentecarmona@gmail.com>
2022-11-30 09:26:23 -0600

    Se guarda la uri localmente, hay un error con el intent de la galeria, guarda la uri pero no guarda la imagen en el previsualizador

--- app/src/main/java/com/example/notestask/Fragmentos/FragmentoAgregarImagenes.kt
+++ app/src/main/java/com/example/notestask/Fragmentos/FragmentoAgregarImagenes.kt
@@ -4,19 +4,26 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.FileProvider
 import com.example.notestask.BaseDatos.BaseDatosNotas
 import com.example.notestask.Entidades.Imagenes
 import com.example.notestask.R
 import kotlinx.android.synthetic.main.f_agregar_imagenes.*
 import kotlinx.coroutines.launch
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FragmentoAgregarImagenes : FragmentoBase() {
     private var idN = -1
     private var urImagen: Uri? = null
     private val SELECT_ACTIVITY = 50
+    private val TAKE_ACTIVITY = 100
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         idN = requireArguments().getInt("idN", -1)
@@ -39,12 +46,52 @@ class FragmentoAgregarImagenes : FragmentoBase() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         btnAgregarFotoCN.setOnClickListener {
             //   ImageControler.selectPhotoFromGallery(this, SELECT_ACTIVITY)
             val intent = Intent(Intent.ACTION_PICK)
             intent.type = "image/*"
-            this.startActivityForResult(intent, SELECT_ACTIVITY)
+            //this.startActivityForResult(intent, SELECT_ACTIVITY)
+            intent.also { pickPictureIntent ->
+                pickPictureIntent.resolveActivity(requireActivity().packageManager).also {
+                    val imageFile: File? = try {
+                        createImageFile()
+                    } catch (ex: IOException) {
+                        null
+                    }
+                    imageFile?.also {
+                        urImagen = FileProvider.getUriForFile(
+                            requireContext(), "com.example.notestask.fileprovider", it
+                        )
+
+                        startActivityForResult(pickPictureIntent, SELECT_ACTIVITY)
+                    }
+                }
+            }
+
         }
+
+        btnAgregarFotoCamaraCN.setOnClickListener {
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+                takePictureIntent.resolveActivity(requireActivity().packageManager)?.also {
+                    val photoFile: File? = try {
+                        createImageFile()
+                    } catch (ex: IOException) {
+
+                        null
+                    }
+                    photoFile?.also {
+                        urImagen = FileProvider.getUriForFile(
+                            requireContext(), "com.example.notestask.fileprovider", it
+                        )
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, urImagen)
+                        startActivityForResult(takePictureIntent, TAKE_ACTIVITY)
+                    }
+                }
+
+            }
+        }
+
         btnGuardarCN.setOnClickListener {
             guardarImagen()
         }
@@ -67,13 +114,33 @@ class FragmentoAgregarImagenes : FragmentoBase() {
         }
     }
 
+    lateinit var currentPhotoPath: String
+    lateinit var currentVideoPath: String
+
+    @Throws(IOException::class)
+    fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        //val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val storageDir: File? = context?.filesDir
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+            currentVideoPath = absolutePath
+        }
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        when {
-            requestCode == SELECT_ACTIVITY && resultCode == Activity.RESULT_OK -> {
-                urImagen = data!!.data
-                mostrarFotoCN.setImageURI(urImagen)
-            }
+
+        if (requestCode == SELECT_ACTIVITY && resultCode == Activity.RESULT_OK) {
+            mostrarFotoCN.setImageURI(urImagen)
+        } else if (requestCode == TAKE_ACTIVITY && resultCode == Activity.RESULT_OK) {
+            mostrarFotoCN.setImageURI(urImagen)
         }
     }
 }
