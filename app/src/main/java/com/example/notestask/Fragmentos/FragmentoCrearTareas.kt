61fe84b038c14454586cec41b9591caeeabfdd91
Clementain <clentecarmona@gmail.com>
2022-11-30 18:55:01 -0600

    Optimizacion de la clase del FragmentoCrearTareas.kt (persiste el error de la uri)

--- app/src/main/java/com/example/notestask/Fragmentos/FragmentoCrearTareas.kt
+++ app/src/main/java/com/example/notestask/Fragmentos/FragmentoCrearTareas.kt
@@ -1,12 +1,6 @@
 package com.example.notestask.Fragmentos
 
-import android.Manifest
-import android.app.Activity.RESULT_OK
-import android.content.Intent
-import android.graphics.BitmapFactory
-import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,18 +11,12 @@ import com.example.notestask.R
 import kotlinx.android.synthetic.main.f_crear_tareas.*
 import kotlinx.android.synthetic.main.f_vista_tareas.*
 import kotlinx.coroutines.launch
-import pub.devrel.easypermissions.AppSettingsDialog
-import pub.devrel.easypermissions.EasyPermissions
 import java.text.SimpleDateFormat
 import java.util.*
 
-class FragmentoCrearTareas : FragmentoBase(), EasyPermissions.PermissionCallbacks,
-    EasyPermissions.RationaleCallbacks {
+class FragmentoCrearTareas : FragmentoBase() {
 
     var currentDate: String? = null
-    private var READ_STORAGE_PERM = 123
-    private var REQUEST_CODE_IMAGE = 456
-    private var selectedImagePath = ""
     private var taskId = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -148,101 +136,6 @@ class FragmentoCrearTareas : FragmentoBase(), EasyPermissions.PermissionCallback
         }
     }
 
-
-    private fun hasReadStoragePerm(): Boolean {
-        return EasyPermissions.hasPermissions(
-            requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE
-        )
-    }
-
-
-    private fun readStorageTask() {
-        if (hasReadStoragePerm()) {
-
-
-            pickImageFromGallery()
-        } else {
-            EasyPermissions.requestPermissions(
-                requireActivity(),
-                getString(R.string.storage_permission_text),
-                READ_STORAGE_PERM,
-                Manifest.permission.READ_EXTERNAL_STORAGE
-            )
-        }
-    }
-
-    private fun pickImageFromGallery() {
-        var intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        if (intent.resolveActivity(requireActivity().packageManager) != null) {
-            startActivityForResult(intent, REQUEST_CODE_IMAGE)
-        }
-    }
-
-    private fun getPathFromUri(contentUri: Uri): String? {
-        var filePath: String? = null
-        var cursor = requireActivity().contentResolver.query(contentUri, null, null, null, null)
-        if (cursor == null) {
-            filePath = contentUri.path
-        } else {
-            cursor.moveToFirst()
-            var index = cursor.getColumnIndex("_data")
-            filePath = cursor.getString(index)
-            cursor.close()
-        }
-        return filePath
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_CODE_IMAGE && resultCode == RESULT_OK) {
-            if (data != null) {
-                var selectedImageUrl = data.data
-                if (selectedImageUrl != null) {
-                    try {
-                        var inputStream =
-                            requireActivity().contentResolver.openInputStream(selectedImageUrl)
-                        var bitmap = BitmapFactory.decodeStream(inputStream)
-
-
-                        selectedImagePath = getPathFromUri(selectedImageUrl)!!
-                    } catch (e: Exception) {
-                        Toast.makeText(requireContext(), e.message, Toast.LENGTH_SHORT).show()
-                    }
-
-                }
-            }
-        }
-    }
-
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        EasyPermissions.onRequestPermissionsResult(
-            requestCode, permissions, grantResults, requireActivity()
-        )
-    }
-
-
-    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
-        if (EasyPermissions.somePermissionPermanentlyDenied(requireActivity(), perms)) {
-            AppSettingsDialog.Builder(requireActivity()).build().show()
-        }
-    }
-
-    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
-
-    }
-
-    override fun onRationaleDenied(requestCode: Int) {
-
-    }
-
-    override fun onRationaleAccepted(requestCode: Int) {
-
-    }
 
 }
 
