2bb4a8dc1c1a759a80891b0f1e7e7895bb39d048
JoseMRM210801 <s19120164@alumnos.itsur.edu.mx>
2022-11-30 19:58:28 -0600

    se agregan los permisos para el uso de la aplicacion(persiste el error de la uri)

--- app/src/main/java/com/example/notestask/Fragmentos/MainActivity.kt
+++ app/src/main/java/com/example/notestask/Fragmentos/MainActivity.kt
@@ -1,17 +1,220 @@
 package com.example.notestask
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        revisarPermisos()
         replaceFragment(FragmentoInicio.newInstance(), false)
 
+    }
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun revisarPermisos() {
+        when {
+            ContextCompat.checkSelfPermission(
+                applicationContext,
+                "android.permission.RECORD_AUDIO"
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // You can use the API that requires the permission.
+
+            }
+            shouldShowRequestPermissionRationale("android.permission.RECORD_AUDIO") -> {
+                // In an educational UI, explain to the user why your app requires this
+                // permission for a specific feature to behave as expected. In this UI,
+                // include a "cancel" or "no thanks" button that allows the user to
+                // continue using your app without granting the permission.
+                //showInContextUI(...)
+                //Toast.makeText(applicationContext, "Debes dar perimso para grabar audios", Toast.LENGTH_SHORT).show()
+                MaterialAlertDialogBuilder(this
+                )
+                    .setTitle("Title")
+                    .setMessage("Debes dar perimso para grabar audios")
+                    .setNegativeButton("Cancel") { dialog, which ->
+                        // Respond to negative button press
+                    }
+                    .setPositiveButton("OK") { dialog, which ->
+                        // Respond to positive button press
+                        /*requestPermissionLauncher.launch(
+                            "android.permission.RECORD_AUDIO")*/
+
+                        // You can directly ask for the permission.
+                        requestPermissions(
+                            arrayOf("android.permission.RECORD_AUDIO","",
+                                "android.permission.WRITE_EXTERNAL_STORAGE"),
+                            1001)
+
+                    }
+                    .show()
+            }
+            shouldShowRequestPermissionRationale("android.permission.RECORD_AUDIO") -> {
+                // In an educational UI, explain to the user why your app requires this
+                // permission for a specific feature to behave as expected. In this UI,
+                // include a "cancel" or "no thanks" button that allows the user to
+                // continue using your app without granting the permission.
+                //showInContextUI(...)
+                //Toast.makeText(applicationContext, "Debes dar perimso para grabar audios", Toast.LENGTH_SHORT).show()
+                MaterialAlertDialogBuilder(this
+                )
+                    .setTitle("Title")
+                    .setMessage("Debes dar perimso para acceder al almacenamiento")
+                    .setNegativeButton("Cancel") { dialog, which ->
+                        // Respond to negative button press
+                    }
+                    .setPositiveButton("OK") { dialog, which ->
+                        // Respond to positive button press
+                        /*requestPermissionLauncher.launch(
+                            "android.permission.RECORD_AUDIO")*/
+
+                        // You can directly ask for the permission.
+                        requestPermissions(
+                            arrayOf("android.permission.READ_EXTERNAL_STORAGE"
+                               ),
+                            1003)
+
+                    }
+                    .show()
+            }
+            shouldShowRequestPermissionRationale("android.permission.CAMERA") -> {
+                // In an educational UI, explain to the user why your app requires this
+                // permission for a specific feature to behave as expected. In this UI,
+                // include a "cancel" or "no thanks" button that allows the user to
+                // continue using your app without granting the permission.
+                //showInContextUI(...)
+                //Toast.makeText(applicationContext, "Debes dar perimso para grabar audios", Toast.LENGTH_SHORT).show()
+                MaterialAlertDialogBuilder(this
+                )
+                    .setTitle("Title")
+                    .setMessage("Debes dar perimso para usar la camara")
+                    .setNegativeButton("Cancel") { dialog, which ->
+                        // Respond to negative button press
+                    }
+                    .setPositiveButton("OK") { dialog, which ->
+                        // Respond to positive button press
+                        /*requestPermissionLauncher.launch(
+                            "android.permission.RECORD_AUDIO")*/
+
+                        // You can directly ask for the permission.
+                        requestPermissions(
+                            arrayOf("android.permission.CAMERA",
+                                "android.permission.WRITE_EXTERNAL_STORAGE"),
+                            1002)
+
+                    }
+                    .show()
+            }
+
+
+            else -> {
+                // You can directly ask for the permission.
+                // The registered ActivityResultCallback gets the result of this request.
+                /*requestPermissionLauncher.launch(
+                    "android.permission.RECORD_AUDIO")*/
+                requestPermissions(
+                    arrayOf("android.permission.RECORD_AUDIO","android.permission.CAMERA",
+                        "android.permission.READ_EXTERNAL_STORAGE" ,
+                        "android.permission.WRITE_EXTERNAL_STORAGE"),
+                    1004)
+            }
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1001 -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() &&
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                            grantResults[1] == PackageManager.PERMISSION_GRANTED
+                            )) {
+                    // Permission is granted. Continue the action or workflow
+                    // in your app.
+                    replaceFragment(FragmentoInicio.newInstance(), false)
+                } else {
+                    // Explain to the user that the feature is unavailable because
+                    // the features requires a permission that the user has denied.
+                    // At the same time, respect the user's decision. Don't link to
+                    // system settings in an effort to convince the user to change
+                    // their decision.
+                }
+                return
+            }
+            1002 -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() &&
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                            grantResults[1] == PackageManager.PERMISSION_GRANTED
+                            )) {
+                    // Permission is granted. Continue the action or workflow
+                    // in your app.
+                    replaceFragment(FragmentoInicio.newInstance(), false)
+                } else {
+                    // Explain to the user that the feature is unavailable because
+                    // the features requires a permission that the user has denied.
+                    // At the same time, respect the user's decision. Don't link to
+                    // system settings in an effort to convince the user to change
+                    // their decision.
+                }
+                return
+            }
+
+            1003 -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() &&
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED
+                            )) {
+                    // Permission is granted. Continue the action or workflow
+                    // in your app.
+                    replaceFragment(FragmentoInicio.newInstance(), false)
+                } else {
+                    // Explain to the user that the feature is unavailable because
+                    // the features requires a permission that the user has denied.
+                    // At the same time, respect the user's decision. Don't link to
+                    // system settings in an effort to convince the user to change
+                    // their decision.
+                }
+                return
+            }
+            1004 -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() &&
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                            grantResults[1] == PackageManager.PERMISSION_GRANTED &&
+                            grantResults[2] == PackageManager.PERMISSION_GRANTED &&
+                            grantResults[3] == PackageManager.PERMISSION_GRANTED
+                            )) {
+                    // Permission is granted. Continue the action or workflow
+                    // in your app.
+                    replaceFragment(FragmentoInicio.newInstance(), false)
+                } else {
+                    // Explain to the user that the feature is unavailable because
+                    // the features requires a permission that the user has denied.
+                    // At the same time, respect the user's decision. Don't link to
+                    // system settings in an effort to convince the user to change
+                    // their decision.
+                }
+                return
+            }
+            // Add other 'when' lines to check for other
+            // permissions this app might request.
+            else -> {
+                // Ignore all other requests.
+            }
+        }
     }
 
 
